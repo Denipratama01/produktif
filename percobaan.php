0fb634536cfe1ccdc7ecc11c58dc9d0430360361
Denipratama01 <denipratama4615@gmail.com>
2024-02-04 05:08:07 -0800

    First Upload

---
+++ percobaan.php
@@ -0,0 +1,14 @@
+<?php
+    $nama="Deni ";
+    $nim="E32231651 ";
+    $prodi="teknik Komputer ";
+    $umur="18 ";
+    $alamat="Karang Anom ";
+    echo "selamat praktikum ";
+    echo $nama;
+    echo $nim;
+    echo $prodi;
+    echo $umur;
+    echo $alamat;
+    echo "nama = ".$nama;
+    ?>
